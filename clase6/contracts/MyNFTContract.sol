8c53689a23acf7e45b06a34296893efac1d84bfe
Gringo <m_nivoli@hotmail.com>
2024-06-07 11:17:22 -0300

    Feat: Add clase 6

---
+++ clase6/contracts/MyNFTContract.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyNFTContract is ERC721, ERC721URIStorage, Ownable {
+
+    IERC20 private _erc20Token;
+
+    constructor(address initialOwner, address erc20Token) 
+        ERC721("MyNFTContract", "MNC") 
+        Ownable(initialOwner) 
+    {
+        _erc20Token = IERC20(erc20Token);
+    }
+
+    function mint(address to, uint tokenId, string memory uri) public {
+        if (_erc20Token.balanceOf(msg.sender) < 1e18) {
+            revert(); // podríamos agregar un error
+        }
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns(string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns(bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
